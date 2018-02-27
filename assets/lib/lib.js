69f861d8acfc666fc93649a9772c13f6a98b470a
zhonglouguairen <409158846@qq.com>
2018-02-27 13:59:42 +0800

    拆分提交

---
+++ assets/lib/lib.js
@@ -0,0 +1,8 @@
+document.write('<scr' + 'ipt type="text/javascript" src="'+'../lib/jquery-3.2.1.min.js'+'"></scr' + 'ipt>');
+document.write('<scr' + 'ipt type="text/javascript" src="'+'../lib/vue.min.js'+'"></scr' + 'ipt>');
+document.write('<scr' + 'ipt type="text/javascript" src="'+'../lib/elementUI.js'+'"></scr' + 'ipt>');
+document.write('<scr' + 'ipt type="text/javascript" src="'+'../lib/axios.min.js'+'"></scr' + 'ipt>');
+document.write('<scr' + 'ipt type="text/javascript" src="'+'../lib/vue-resource.min.js'+'"></scr' + 'ipt>');
+
+
+
