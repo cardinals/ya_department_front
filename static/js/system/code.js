a67f8c5ff09e983e7d5482b70cc172c1a0ec6502
huang-rui <hr18904047625@163.com>
2018-03-08 15:45:14 +0800

    修改页提交验证

--- static/js/system/code.js
+++ static/js/system/code.js
@@ -1,8 +1,8 @@
 //axios默认设置cookie
-axios.defaults.withCredentials = true;  
+axios.defaults.withCredentials = true;
 new Vue({
     el: '#app',
-    data: function() {
+    data: function () {
         return {
             visible: false,
             //搜索表单
@@ -13,7 +13,7 @@ new Vue({
             },
             tableData: [],
             //表高度变量
-            tableheight :445,
+            tableheight: 445,
             //显示加载中样
             loading: false,
             //多选值
@@ -23,20 +23,22 @@ new Vue({
             //分页大小
             pageSize: 10,
             //总记录数
-            total:10,
+            total: 10,
             //新建页面是否显示
-            addFormVisible:false,
-            addFormRules:{
+            addFormVisible: false,
+            addFormRules: {
                 codetype: [{ required: true, message: "请输入代码集类型", trigger: "blur" }],
                 codetypeName: [{ required: true, message: "请输入代码集类型名称", trigger: "blur" }]
             },
             //新建数据
-            addForm:{
+            addForm: {
                 permissionname: "",
                 permissioninfo: ""
             },
             //选中的序号
             selectIndex: -1,
+            //修改界面表单值是否发生改变
+            editFormFlag: false,
             //修改界面是否显示
             editFormVisible: false,
             editFormRules: {
@@ -46,14 +48,23 @@ new Vue({
             //修改界面数据
             editForm: {
                 codetype: "",
-                codetypeName: ""
+                codetypeName: "",
+                remark: "",
+                language: ""
             }
         }
+    },
+    //表单值变化验证
+    watch: {
+        editForm: function (val) {
+            this.editFormFlag = true;
+        },
+        deep: true
     },
     created: function () {
         this.searchClick();
     },
-    methods:{
+    methods: {
         handleNodeClick(data) {
             console.log(data);
         },
@@ -67,13 +78,13 @@ new Vue({
         },
 
         //表格中日期格式化
-        dateFormat: function(row, column){
+        dateFormat: function (row, column) {
             var rowDate = row[column.property];
-            if(rowDate == null || rowDate == ""){
+            if (rowDate == null || rowDate == "") {
                 return '';
-            }else{
+            } else {
                 var date = new Date(rowDate);
-                if(date == undefined){
+                if (date == undefined) {
                     return '';
                 }
                 var month = '' + (date.getMonth() + 1),
@@ -87,14 +98,14 @@ new Vue({
             }
         },
 
-        codetypeCilck: function(val){
-            window.location.href = "/templates/system/codelist_detail.html" + "?codeid=" + val.codeid;
+        codetypeCilck: function (val) {
+            window.location.assign("/templates/system/codelist_detail.html?codeid=" + val.codeid);
         },
 
         //查询，初始化
-        searchClick: function() {
+        searchClick: function () {
             var _self = this;
-            if(this.searchForm.createTimeBegin!="" && this.searchForm.createTimeEnd!="" && this.searchForm.createTimeBegin>this.searchForm.createTimeEnd){
+            if (this.searchForm.createTimeBegin != "" && this.searchForm.createTimeEnd != "" && this.searchForm.createTimeBegin > this.searchForm.createTimeEnd) {
                 _self.$message({
                     message: "时间选择错误！",
                     type: "error"
@@ -107,66 +118,66 @@ new Vue({
                 createTimeEnd: this.searchForm.createTimeEnd
             };
 
-            axios.post('/api/codelist/findByVO', params).then(function(res){
+            axios.post('/api/codelist/findByVO', params).then(function (res) {
                 this.tableData = res.data.result;
                 this.total = res.data.result.length;
-            }.bind(this),function(error){
+            }.bind(this), function (error) {
                 console.log(error)
             })
         },
 
         //表格勾选事件
-        selectionChange: function(val) {
+        selectionChange: function (val) {
             this.multipleSelection = val;
         },
 
         //表格重新加载数据
-        loadingData: function() {
+        loadingData: function () {
             var _self = this;
             _self.loading = true;
-            setTimeout(function() {
+            setTimeout(function () {
                 _self.loading = false;
             }, 300);
         },
 
         //新建：弹出Dialog
-        addClick: function() {
+        addClick: function () {
             var _self = this;
             _self.addFormVisible = true;
         },
 
         //新建：保存
-        addSubmit: function(val) {
-            var _self=this;
-            axios.get('/api/codelist/getNum/' + this.addForm.codetype).then(function(res){
-                if(res.data.result != 0){
+        addSubmit: function (val) {
+            var _self = this;
+            axios.get('/api/codelist/getNum/' + this.addForm.codetype).then(function (res) {
+                if (res.data.result != 0) {
                     _self.$message({
                         message: "代码集类型已存在!",
                         type: "error"
                     });
-                }else{
+                } else {
                     var params = {
                         codetype: val.codetype.trim(),
                         codetypeName: val.codetypeName.trim()
                     }
-                    axios.post('/api/codelist/insertByVO', params).then(function(res){
+                    axios.post('/api/codelist/insertByVO', params).then(function (res) {
                         var addData = res.data.result;
                         addData.createTime = new Date();
                         _self.tableData.unshift(addData);
                         _self.total = _self.tableData.length;
-                    }.bind(this),function(error){
+                    }.bind(this), function (error) {
                         console.log(error)
                     })
                     this.addFormVisible = false;
                     _self.loadingData();//重新加载数据
                 }
-            }.bind(this),function(error){
+            }.bind(this), function (error) {
                 console.log(error)
             })
         },
 
         //修改：弹出Dialog
-        editClick: function() {
+        editClick: function () {
             var _self = this;
             var multipleSelection = this.multipleSelection;
             if (multipleSelection.length < 1) {
@@ -176,10 +187,10 @@ new Vue({
                 });
                 return;
             }
-            else if(multipleSelection.length>1){
+            else if (multipleSelection.length > 1) {
                 _self.$message({
-                    message:"只能选一条记录进行编辑",
-                    type:"error"
+                    message: "只能选一条记录进行编辑",
+                    type: "error"
                 });
                 return;
             }
@@ -187,8 +198,8 @@ new Vue({
             var codeid = multipleSelection[0].codeid;
 
             //获取选择的行号
-            for(var k=0;k< _self.tableData.length;k++) {
-                if(_self.tableData[k].codeid == codeid){
+            for (var k = 0; k < _self.tableData.length; k++) {
+                if (_self.tableData[k].codeid == codeid) {
                     _self.selectIndex = k;
                 }
             }
@@ -196,32 +207,43 @@ new Vue({
             //直接从table中取值放在form表单中
             this.editForm = Object.assign({}, _self.tableData[_self.selectIndex]);
             this.editFormVisible = true;
+            this.editFormFlag = false;
         },
 
         //修改：保存
-        editSubmit: function(val) {
-            var params = {
-                codeid: val.codeid,
-                codetype: val.codetype.trim(),
-                codetypeName: val.codetypeName.trim(),
-                remark: val.remark,
-                language: val.language
-            };
-            axios.post('/api/codelist/updateByVO', params).then(function(res){
-                this.tableData[this.selectIndex].codetype = val.codetype;
-                this.tableData[this.selectIndex].codetypeName = val.codetypeName;
-                this.tableData[this.selectIndex].remark = val.remark;
-                this.tableData[this.selectIndex].language = val.language;
-                this.tableData[this.selectIndex].alterName = res.data.result.alterName;
-                this.tableData[this.selectIndex].alterTime = new Date();
-            }.bind(this),function(error){
-                console.log(error)
-            })
-            this.editFormVisible = false;
+        editSubmit: function (val) {
+            if (this.editFormFlag) {
+                var params = {
+                    codeid: val.codeid,
+                    codetype: val.codetype.trim(),
+                    codetypeName: val.codetypeName.trim(),
+                    remark: val.remark,
+                    language: val.language
+                };
+                axios.post('/api/codelist/updateByVO', params).then(function (res) {
+                    this.tableData[this.selectIndex].codetype = val.codetype;
+                    this.tableData[this.selectIndex].codetypeName = val.codetypeName;
+                    this.tableData[this.selectIndex].remark = val.remark;
+                    this.tableData[this.selectIndex].language = val.language;
+                    this.tableData[this.selectIndex].alterName = res.data.result.alterName;
+                    this.tableData[this.selectIndex].alterTime = new Date();
+                }.bind(this), function (error) {
+                    console.log(error)
+                })
+                this.editFormVisible = false;
+                this.editFormFlag = false;
+            }
+            else {
+                this.$message({
+                    message: "数据未发生改动",
+                    type: "error"
+                });
+                return;
+            }
         },
 
         //删除:批量删除
-        removeSelection: function() {
+        removeSelection: function () {
             var _self = this;
             var multipleSelection = this.multipleSelection;
             if (multipleSelection.length < 1) {
@@ -237,16 +259,16 @@ new Vue({
                 var row = multipleSelection[i];
                 ids.push(row.codeid);
             }
-            this.$confirm("确认删除吗？", "提示", {type: "warning"})
-                .then(function() {
+            this.$confirm("确认删除吗？", "提示", { type: "warning" })
+                .then(function () {
                     var params = {
                         ids: ids
                     }
-                    axios.post('/api/codelist/deleteByIds', params).then(function(res){
-                        for(var d =0;d< ids.length;d++){
-                            for(var k=0;k< _self.tableData.length;k++) {
-                                if(_self.tableData[k].codeid == ids[d]){
-                                    _self.tableData.splice(k,1);
+                    axios.post('/api/codelist/deleteByIds', params).then(function (res) {
+                        for (var d = 0; d < ids.length; d++) {
+                            for (var k = 0; k < _self.tableData.length; k++) {
+                                if (_self.tableData[k].codeid == ids[d]) {
+                                    _self.tableData.splice(k, 1);
                                 }
                             }
                         }
@@ -256,24 +278,24 @@ new Vue({
                         });
                         _self.total = _self.tableData.length;
                         _self.loadingData(); //重新加载数据
-                    }.bind(this),function(error){
+                    }.bind(this), function (error) {
                         console.log(error)
                     })
 
                 })
-                .catch(function(e) {
+                .catch(function (e) {
                     if (e != "cancel") console.log("出现错误：" + e);
                 });
         },
         //分页大小修改事件
-        pageSizeChange: function(val) {
+        pageSizeChange: function (val) {
             console.log("每页 " + val + " 条");
             this.pageSize = val;
             var _self = this;
             _self.loadingData(); //重新加载数据
         },
         //当前页修改事件
-        currentPageChange: function(val) {
+        currentPageChange: function (val) {
             this.currentPage = val;
             console.log("当前页: " + val);
             var _self = this;
@@ -281,11 +303,13 @@ new Vue({
         },
 
 
-        closeDialog:function(val){
+        closeDialog: function (val) {
             this.addFormVisible = false;
+            this.editFormVisible = false;
             val.permissionname = "";
             val.permissioninfo = "";
-            this.$refs["addForm"].resetFields();
+            this.$refs[val].resetFields();
+            this.editFormFlag = false;
         }
     },
 })
