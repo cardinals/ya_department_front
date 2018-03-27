1ef0f641c1a6dc8573a46c77241017faabaa6301
zhonglouguairen <409158846@qq.com>
2018-03-27 15:28:11 +0800

    预案管理优化修改

--- static/js/digitalplan/digitalplanlist.js
+++ static/js/digitalplan/digitalplanlist.js
@@ -169,7 +169,6 @@ new Vue({
             addFormVisible: false,
             addLoading: false,
             addFormRules: {
-                permissionname: [{ required: true, message: "请输入权限名称", trigger: "blur" }]
             },
             //新建数据
             addForm: {
@@ -189,7 +188,6 @@ new Vue({
             editFormVisible: false,
             editLoading: false,
             editFormRules: {
-                permissionname: [{ required: true, message: "请输入角色名称", trigger: "blur" }]
             },
             //编辑界面数据
             planDetail: {
@@ -416,74 +414,7 @@ new Vue({
                 _self.loading = false;
             }, 300);
         },
-
-        //新建事件
-        addClick: function () {
-            var _self = this;
-            _self.addFormVisible = true;
-
-        },
-        //新建提交点击事件
-        addSubmit: function (val) {
-            var _self = this;
-            this.tableData.unshift({
-                permissionname: val.permissionname,
-                permissioninfo: val.permissioninfo,
-                create_name: val.create_name,
-                create_time: val.create_time,
-                alter_name: val.alter_name,
-                alter_time: val.alter_time
-            });
-            this.addFormVisible = false;
-            _self.total = _self.tableData.length;
-            _self.loadingData();//重新加载数据
-            val.permissionname = "";
-            val.permissioninfo = "";
-            val.create_name = "";
-            val.create_time = "";
-            val.alter_name = "";
-            val.alter_time = "";
-            console.info(this.addForm);
-
-        },
-        //删除所选，批量删除
-        removeSelection: function () {
-            var _self = this;
-            var multipleSelection = this.multipleSelection;
-            if (multipleSelection.length < 1) {
-                _self.$message({
-                    message: "请至少选中一条记录",
-                    type: "error"
-                });
-                return;
-            }
-            var ids = [];
-            for (var i = 0; i < multipleSelection.length; i++) {
-                var row = multipleSelection[i];
-                ids.push(row.permissionname);
-            }
-            this.$confirm("确认删除" + ids + "吗?", "提示", {
-                type: "warning"
-            })
-                .then(function () {
-                    for (var d = 0; d < ids.length; d++) {
-                        for (var k = 0; k < _self.tableData.length; k++) {
-                            if (_self.tableData[k].permissionname == ids[d]) {
-                                _self.tableData.splice(k, 1);
-                            }
-                        }
-                    }
-                    _self.$message({
-                        message: ids + "删除成功",
-                        type: "success"
-                    });
-                    _self.total = _self.tableData.length;
-                    _self.loadingData(); //重新加载数据
-                })
-                .catch(function (e) {
-                    if (e != "cancel") console.log("出现错误：" + e);
-                });
-        },
+       
         //分页大小修改事件
         pageSizeChange: function (val) {
             console.log("每页 " + val + " 条");
@@ -497,55 +428,6 @@ new Vue({
             console.log("当前页: " + val);
             var _self = this;
             _self.loadingData(); //重新加载数据
-        },
-        //表格编辑事件
-        editClick: function () {
-            var _self = this;
-            var multipleSelection = this.multipleSelection;
-            if (multipleSelection.length < 1) {
-                _self.$message({
-                    message: "请至少选中一条记录",
-                    type: "error"
-                });
-                return;
-            }
-            else if (multipleSelection.length > 1) {
-                _self.$message({
-                    message: "只能选一条记录进行编辑",
-                    type: "error"
-                });
-                return;
-            }
-            //var ids = "";
-            var ids = [];
-            for (var i = 0; i < multipleSelection.length; i++) {
-                var row = multipleSelection[i];
-                //ids += row.realname + ",";
-                ids.push(row.permissionname);
-            }
-            for (var d = 0; d < ids.length; d++) {
-                for (var k = 0; k < _self.tableData.length; k++) {
-                    if (_self.tableData[k].permissionname == ids[d]) {
-                        _self.selectIndex = k;
-                    }
-                }
-            }
-            this.editForm = Object.assign({}, _self.tableData[_self.selectIndex]);
-            //this.editForm.sex=(row.sex == "男"?1:0);
-            this.editFormVisible = true;
-        },
-        //保存点击事件
-        editSubmit: function (val) {
-            var _self = this;
-            this.tableData[this.selectIndex].permissionname = val.permissionname;
-            this.tableData[this.selectIndex].permissioninfo = val.permissioninfo;
-            this.tableData[this.selectIndex].create_name = val.create_name;
-            this.tableData[this.selectIndex].create_time = val.create_time;
-            this.tableData[this.selectIndex].alter_name = val.alter_name;
-            this.tableData[this.selectIndex].alter_time = val.alter_time;
-            this.editFormVisible = false;
-            _self.loadingData();//重新加载数据
-            console.info(this.editForm);
         },
         closeDialog: function (val) {
             this.planDetailVisible = false;
