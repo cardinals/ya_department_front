e3648fa1c634a515b7c55b50087eeceb1fae1c71
zhonglouguairen <409158846@qq.com>
2018-05-18 13:50:13 +0800

    化危品和火场样式修改

--- static/js/auxiliarydecision/danger_list.js
+++ static/js/auxiliarydecision/danger_list.js
@@ -102,8 +102,28 @@ new Vue({
             this.searchForm.EXPRESSION="";
         },
         getLXDMData: function (){
+            var LXDM = [];
             axios.get('/api/codelist/getCodetype/HXWXPLX').then(function(res){
-                this.LXDM_data=res.data.result;
+                LXDM = res.data.result;
+                for(var i = 0; i<LXDM.length;i++){
+                    LXDM[i].codeValue=parseInt(LXDM[i].codeValue);
+                    this.LXDM_data.push(LXDM[i]);
+                }
+                //定义一个比较器
+                function compare(propertyName) {
+                    return function(object1, object2) {
+                    var value1 = object1[propertyName];
+                    var value2 = object2[propertyName];
+                    if (value2 < value1) {
+                        return 1;
+                    } else if (value2 > value1) {
+                        return -1;
+                    } else {
+                        return 0;
+                    }
+                    }
+                }
+                this.LXDM_data.sort(compare("codeValue"));
             }.bind(this),function(error){
                 console.log(error);
             })
