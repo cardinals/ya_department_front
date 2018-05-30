6fbc42a4971bafb05332964aec64f156ed5a1f62
lixue0919 <442019696@qq.com>
2018-05-30 13:15:43 +0800

    消防保卫警卫预案功能及样式调整

--- static/js/digitalplan/guardobjectsplan_detail.js
+++ static/js/digitalplan/guardobjectsplan_detail.js
@@ -67,6 +67,16 @@ new Vue({
                 axios.get('/dpapi/xfbwjw/doFindById/' + val).then(function (res) {
                     this.detailData = null;
                     this.detailData = res.data.result;
+
+                    //显示图片
+                    var photo64 = this.detailData.photo64;
+                    var photo = document.getElementById("photo");
+                    if(photo64 == "" || photo64 == null){
+                        photo.src = "../../static/images/no-picture.png";
+                    }else{
+                        photo.src = "data:image/png;base64,"+photo64;
+                    }
+
                     //制作时间
                     if (this.detailData.zzsj == null || this.detailData.zzsj == "") {
                         this.detailData.zzsj='';
