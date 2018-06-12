8318095cfa19261f61bff750367ab7c1fabb4f81
rliu666 <rliu666@sina.cn>
2018-06-12 14:21:48 +0800

    药剂变更

--- static/js/basicinfo/firedrug_list.js
+++ static/js/basicinfo/firedrug_list.js
@@ -1,5 +1,5 @@
 //加载面包屑
-window.onload=function(){
+window.onload = function () {
     loadBreadcrumb("消防药剂管理", "-1");
 }
 //axios默认设置cookie
@@ -9,20 +9,23 @@ new Vue({
     data: function () {
         return {
             visible: false,
+            drugDetailVisible: false,
             //搜索表单
             searchForm: {
                 ssdz: "",
-                yjlx: "",
+                yjlx: [],
                 cbl: [0, 1000],
                 czl: [0, 1000],
             },
             tableData: [],
+            tableData_detail: {},
             allYjlxDataTree: [],//药剂类型级联选择器数据
             allSsdzData: [],//所属队站下拉框数据
             //表高度变量
             tableheight: 450,
             //显示加载中样
             loading: false,
+            loading_detail: false,
             labelPosition: 'right',
             //多选值
             multipleSelection: [],
@@ -68,13 +71,9 @@ new Vue({
         },
         //药剂类型级联选择器数据
         getAllYjlxDataTree: function () {
-            var params= {
-                codetype : "YJLX",
-                list : [1,2,4,6,8]
-            };
-            axios.post('/api/codelist/getCodelisttree2',params).then(function(res){
-                this.allYjlxDataTree=res.data.result;
-            }.bind(this),function(error){
+            axios.post('/api/codelist/getYjlxTree/YJLX').then(function (res) {
+                this.allYjlxDataTree = res.data.result;
+            }.bind(this), function (error) {
                 console.log(error);
             })
         },
@@ -90,15 +89,28 @@ new Vue({
         searchClick: function () {
             this.loading = true;
             var _self = this;
+            var cbl_min = this.searchForm.cbl[0];
+            var cbl_max = this.searchForm.cbl[1];
+            if (this.searchForm.cbl[0] == '0' && this.searchForm.cbl[1] == '1000') {
+                cbl_min = '';
+                cbl_max = '';
+            }
+
+            var czl_min = this.searchForm.czl[0];
+            var czl_max = this.searchForm.czl[1];
+            if (this.searchForm.czl[0] == '0' && this.searchForm.czl[1] == '1000') {
+                czl_min = '';
+                czl_max = '';
+            }
             var params = {
                 ssdz: this.searchForm.ssdz,
                 yjlx: this.searchForm.yjlx[this.searchForm.yjlx.length - 1],
-                cbl_min: this.searchForm.cbl[0],
-                cbl_max: this.searchForm.cbl[1],
-                czl_min: this.searchForm.czl[0],
-                czl_max: this.searchForm.czl[1]
+                zcbl_min: cbl_min,
+                zcbl_max: cbl_max,
+                czl_min: czl_min,
+                czl_max: czl_max
             };
-            axios.post('/dpapi/firedrug/findByVO', params).then(function (res) {
+            axios.post('/dpapi/firedrug/list', params).then(function (res) {
                 this.tableData = res.data.result;
                 this.total = res.data.result.length;
                 this.loading = false;
@@ -106,22 +118,40 @@ new Vue({
                 console.log(error);
             })
         },
+        drugDatail: function (val) {
+            this.drugDetailVisible = true;
+            this.loading_detail = true;
+            this.tableData_detail = val;
+            this.tableData_detail.scsj = this.dateFormat(this.tableData_detail.scsj);
+            this.loading_detail = false;
+
+        },
+        closeDialog: function (val) {
+            this.drugDetailVisible = false;
+        },
         //清空
         clearClick: function () {
             this.searchForm.ssdz = "";
-            this.searchForm.yjlx = "";
-            this.searchForm.cbl = [];
-            this.searchForm.czl = []
+            this.searchForm.yjlx = [];
+            this.searchForm.cbl = [0, 1000];
+            this.searchForm.czl = [0, 1000]
         },
 
-        //表格数据格式化
-        dataFormat: function (row, column) {
-            var rowDate = row[column.property];
-            if (rowDate == null || rowDate == "") {
-                return '无';
-            } else {
-                return rowDate;
+        //时间格式化
+        dateFormat: function (val) {
+            var date = new Date(val);
+            if (date == undefined) {
+                return val;
             }
+            var month = '' + (date.getMonth() + 1),
+                day = '' + date.getDate(),
+                year = date.getFullYear();
+
+            if (month.length < 2) month = '0' + month;
+            if (day.length < 2) day = '0' + day;
+
+            var newDate = [year, month, day].join('-');
+            return newDate;
         },
         //表格勾选事件
         selectionChange: function (val) {
