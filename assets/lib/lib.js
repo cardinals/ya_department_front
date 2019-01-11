d75403451869f41a2897240200f43fc9e4dae313
lixue0919 <442019696@qq.com>
2019-01-11 09:41:01 +0800

    删除assets目录

--- assets/lib/lib.js
+++
@@ -1,8 +0,0 @@
-document.write('<scr' + 'ipt type="text/javascript" src="'+'../lib/jquery-3.2.1.min.js'+'"></scr' + 'ipt>');
-document.write('<scr' + 'ipt type="text/javascript" src="'+'../lib/vue.min.js'+'"></scr' + 'ipt>');
-document.write('<scr' + 'ipt type="text/javascript" src="'+'../lib/elementUI.js'+'"></scr' + 'ipt>');
-document.write('<scr' + 'ipt type="text/javascript" src="'+'../lib/axios.min.js'+'"></scr' + 'ipt>');
-document.write('<scr' + 'ipt type="text/javascript" src="'+'../lib/vue-resource.min.js'+'"></scr' + 'ipt>');
-
-
-
